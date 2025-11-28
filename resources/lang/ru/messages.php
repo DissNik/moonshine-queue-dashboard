f0ddbe73d7d169283a0fef821b485efc56395ff4
Alexander Nikushkin <nikushkin@mail.ru>
2025-11-28 23:23:49 +0500

    feat: ru lang

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'error_deleting_failed_jobs' => 'Ошибка при удалении неудачных заданий',
+    'failed_job_has_been_forge' => 'Неудачное задание было забыто',
+    'failed_jobs_have_been_deleted' => 'Неудачные задания были удалены',
+    'failed_to_retry_job' => 'Не удалось повторить задание',
+    'failed_to_retry_jobs' => 'Не удалось повторить задания',
+    'job_has_been_forget' => 'Задание было забыто',
+    'job_has_been_queued_for_retry' => 'Задание поставлено в очередь для повторения',
+    'jobs_has_been_queued_for_retry' => 'Задания поставлены в очередь для повторения',
+    'no_failed_jobs_found' => 'Неудачных заданий не найдено. Отличная работа! 🎉',
+];
